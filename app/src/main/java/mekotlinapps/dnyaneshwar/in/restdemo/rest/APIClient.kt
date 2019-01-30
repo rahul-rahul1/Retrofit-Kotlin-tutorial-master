725758626f03f3c145dd92de7ee10f2ab534cb34
signity <rajinder.k@signitysolutions.in>
2019-01-30 18:04:34 +0530

    30 janauary 2019 Retrofit+Kotlin android--Rahul

--- app/src/main/java/mekotlinapps/dnyaneshwar/in/restdemo/rest/APIClient.kt
+++ app/src/main/java/mekotlinapps/dnyaneshwar/in/restdemo/rest/APIClient.kt
@@ -13,24 +13,5 @@ import okhttp3.logging.HttpLoggingInterceptor
 class APIClient {
 
 
-    companion object {
-
-        val baseURL: String = "https://api.themoviedb.org/3/"
-        var retofit: Retrofit? = null
-
-        val client: Retrofit
-            get() {
-                val interceptor = HttpLoggingInterceptor()
-                interceptor.level = HttpLoggingInterceptor.Level.BODY
-                val client = OkHttpClient.Builder()
-                        .addInterceptor(interceptor).build()
-                if (retofit == null) {
-                    retofit = Retrofit.Builder().client(client)
-                            .baseUrl(baseURL)
-                            .addConverterFactory(GsonConverterFactory.create())
-                            .build()
-                }
-                return retofit!!
-            }
-    }
+    companion object
 }
